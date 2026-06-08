b35eeb31835da0a5d381a044124ddc6364f9b1c8
tsandil <tan.sandil44@gmail.com>
2026-06-08 10:50:16 +0545

    feat: add staging layer with sources, models and schema tests

---
+++ dbt_platform/models/staging/stg_orders.sql
@@ -0,0 +1,21 @@
+with src as (
+    
+    select
+        *
+    from {{ source('raw', 'orders') }}
+
+),
+
+renamed as (
+
+    select
+        id as order_id,
+        customer_id,
+        status,
+        amount,
+        ordered_at
+    from src
+
+)
+
+select * from renamed
